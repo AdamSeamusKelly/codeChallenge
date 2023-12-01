4325319c64f470240242ffe21ff0e044d1aa82f4
Adam Kelly <adamkells.ak@gmail.com>
2023-12-01 19:23:26 +0000

    Adding updates to index.html and style.css files. Also fix the issue with the knockout version. Updated functions in the viewModel.js to fitler tasks by status

--- viewModel.js
+++ viewModel.js
@@ -1,19 +1,55 @@
-$(document).ready(function(){
-  
-    function viewModel() {
-        
-        var self = this;
+function UpdateTask() {
+  var self = this;
 
-        self.day = ko.observable('24');
-        self.month = ko.observable('02');
-        self.year = ko.observable('2012');
+  var projectTasks = [
+    {
+      status: "Todo",
+      name: "Add user",
+      description: "Add user",
+      assigned: "John Doe",
+    },
+    {
+      status: "In progress",
+      name: "Create database",
+      description: "Create database",
+      assigned: "John Smith",
+    },
+    {
+      status: "In progress",
+      name: "Build JSON",
+      description: "Create database",
+      assigned: "John Doe",
+    },
+    {
+      status: "Done",
+      name: "Onboarding",
+      description: "Onboarding",
+      assigned: "John Smith",
+    },
+    {
+      status: "Done",
+      name: "Create database",
+      description: "Create database",
+      assigned: "John Doe",
+    },
+  ];
 
-        self.fullDate = ko.computed(function () {
-            return self.day() + "/" + self.month() + "/" + self.year();
-        });
-    
-    };
-  
-    ko.applyBindings(new viewModel()); 
-  
-  });
+  self.filterTasks = function (status) {
+    switch (status.toLowerCase()) {
+      case "todo":
+        return projectTasks.filter((t) => t.status === "Todo");
+      case "inprogress":
+        return projectTasks.filter((t) => t.status === "In progress");
+      case "done":
+        return projectTasks.filter((t) => t.status === "Done");
+      default:
+        return [];
+    }
+  };
+
+  self.todos = ko.computed(() => this.filterTasks("todo"));
+  self.inprogress = ko.computed(() => this.filterTasks("inprogress"));
+  self.done = ko.computed(() => this.filterTasks("done"));
+}
+
+ko.applyBindings(new UpdateTask());
