0d70a2a6b8db0f99a62c8c8dafd610aa11ba521d
Adam Kelly <adamkells.ak@gmail.com>
2023-12-04 20:08:02 +0000

    Intergated Json server and removed hardcoded data. Reworked Drag and drop feature to update status when moved columns

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # codeChallenge
-Flax and Teal code challenge
+
+Trello Kanban coding task
+
+Trello and Kanban with poulated tasks in the TODO, In progress and Done columns.
+
+Pre populated with task in db.json file. Haven't implement an add feature or delete option.
+
+Known bug for drag and drop - Not working on 1st drop everytime, new status is sent to the API
+can refresh and see it displayed in the correct column, can also drag again and it will work
--- viewModel.js
+++ viewModel.js
@@ -1,55 +1,72 @@
-function UpdateTask() {
+// Constants
+const TASK_BASE_URL = "http://localhost:3000/task/";
+const IN_PROGRESS = "IN_PROGRESS";
+const DONE = "DONE";
+const TODO = "TODO";
+
+function UpdateTask(params) {
   var self = this;
 
-  var projectTasks = [
-    {
-      status: "Todo",
-      name: "Add user",
-      description: "Add user",
-      assigned: "John Doe",
-    },
-    {
-      status: "In progress",
-      name: "Create database",
-      description: "Create database",
-      assigned: "John Smith",
-    },
-    {
-      status: "In progress",
-      name: "Build JSON",
-      description: "Create database",
-      assigned: "John Doe",
-    },
-    {
-      status: "Done",
-      name: "Onboarding",
-      description: "Onboarding",
-      assigned: "John Smith",
-    },
-    {
-      status: "Done",
-      name: "Create database",
-      description: "Create database",
-      assigned: "John Doe",
-    },
-  ];
+  // Observables
+  self.projectTasks = ko.observable();
+  self.todos = ko.observable();
+  self.inprogress = ko.observable();
+  self.done = ko.observable();
 
-  self.filterTasks = function (status) {
-    switch (status.toLowerCase()) {
-      case "todo":
-        return projectTasks.filter((t) => t.status === "Todo");
-      case "inprogress":
-        return projectTasks.filter((t) => t.status === "In progress");
-      case "done":
-        return projectTasks.filter((t) => t.status === "Done");
+  self.filterTasks = function (tasks, status) {
+    switch (status) {
+      case TODO:
+        return tasks.filter((t) => t.status === TODO);
+      case IN_PROGRESS:
+        return tasks.filter((t) => t.status === IN_PROGRESS);
+      case DONE:
+        return tasks.filter((t) => t.status === DONE);
       default:
         return [];
     }
   };
 
-  self.todos = ko.computed(() => this.filterTasks("todo"));
-  self.inprogress = ko.computed(() => this.filterTasks("inprogress"));
-  self.done = ko.computed(() => this.filterTasks("done"));
+  self.allowDrop = function (event) {
+    event.preventDefault();
+    return true;
+  };
+
+  self.updateTask = async function (self, event, newStatus) {
+    event.preventDefault();
+    const taskId = event.originalEvent.dataTransfer.getData("text");
+    console.log(event.originalEvent.dataTransfer);
+
+    console.log({ taskId });
+
+    let task;
+    await $.getJSON(TASK_BASE_URL.concat(taskId)).then((res) => {
+      task = res;
+    });
+
+    const updatedTask = {
+      ...task,
+      status: newStatus,
+    };
+
+    $.ajax({
+      type: "PUT",
+      url: TASK_BASE_URL.concat(taskId),
+      data: updatedTask,
+    });
+
+    self.loadTasks();
+  };
+
+  self.loadTasks = function () {
+    $.getJSON(TASK_BASE_URL, (response) => {
+      self.todos(self.filterTasks(response, TODO));
+      self.inprogress(self.filterTasks(response, IN_PROGRESS));
+      self.done(self.filterTasks(response, DONE));
+    });
+  };
+
+  // Init
+  self.loadTasks();
 }
 
-ko.applyBindings(new UpdateTask());
+$(ko.applyBindings(new UpdateTask()));
